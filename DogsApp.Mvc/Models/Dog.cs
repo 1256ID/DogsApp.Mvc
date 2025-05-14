72e5773f619b88a5c770a117aaf4fa134d42342b
1256ID <112385041+1256ID@users.noreply.github.com>
2025-05-14 13:14:19 +0200

    Adding DogService

--- DogsApp.Mvc/Models/Dog.cs
+++ DogsApp.Mvc/Models/Dog.cs
@@ -3,7 +3,7 @@
     public class Dog
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public int Age { get; set; }
     }
 }
---
+++ DogsApp.Mvc/Services/DogService.cs
@@ -0,0 +1,12 @@
+﻿using DogsApp.Mvc.Models;
+namespace DogsApp.Mvc.Services;
+
+public class DogService
+{
+    private List<Dog> dogs = [];
+
+    public void AddDog(Dog dog) => dogs.Add(dog);
+    public Dog[] GetAllDogs() => dogs.ToArray();
+    public Dog? GetDogById(int id) => dogs.SingleOrDefault(c => c.Id.Equals(id));
+
+}
